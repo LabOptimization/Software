294837de7ff0bb0e7a49192fe1ed01db7ef9886a
Lloyd Ramey <lnr0626@gmail.com>
2015-04-07 21:24:55 -0400

    Cleanup the settings definitions

--- go-server/src/settings/settings.go
+++ go-server/src/settings/settings.go
@@ -1,12 +1,10 @@
 package settings
 
-
-const Port string = "8000"
-const Debug bool = true
-
-// paths relative to golang-server/src/main
-// include trailing slash
-
-const TemplatePath = "../../client/templates/"
-
-const StaticPath = "../../client/static/"
+const (
+    Port = "8000"
+    Debug = true
+    // paths relative to golang-server/src/main
+    // include trailing slash
+    TemplatePath = "../../client/templates"
+    StaticPath = "../../client/static"
+)
