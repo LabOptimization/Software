30f7da7464ba90a299da49611964720656d1fe7f
Conor <conorpp@vt.edu>
2015-04-09 10:27:42 -0400

    started with angular

--- go-server/client/static/js/main.js
+++ go-server/client/static/js/main.js
@@ -1,21 +1,13 @@
-var phonecatApp = angular.module('phonecatApp', []);
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
-      $scope.phones = [
-          {'name': 'Nexus S',
-               'snippet': 'Fast just got faster with Nexus S.'},
-                   {'name': 'Motorola XOOM™ with Wi-Fi',
-                        'snippet': 'The Next, Next Generation tablet.'},
-                            {'name': 'MOTOROLA XOOM™',
-                                 'snippet': 'The Next, Next Generation tablet.'}
-                                   ];
+var voltApp = angular.module('voltApp', []);
+voltApp.controller('PeopleListCtrl', function ($scope) {
+      $scope.jobs = [
+          {'name': 'Joe',
+           'job': 'Plumber'},
+           {'name': 'Mary',
+           'job': 'Pilot'},
+           {'name': 'Bill',
+           'job': 'Unemployed'}
+      ];
 });
 
-/*
-phonecatApp.controller('peopleListCtrl', function ($scope) {
-        $scope.People = [
-            {name: 'Joe', job: 'Plumber'},
-            {name: 'Mary', job: 'Pilot'},
-            {name: 'Will', job: 'unemployed'}
-        ];
-});*/
 
