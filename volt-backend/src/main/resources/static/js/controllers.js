18a11a3ab8fb1ed43adfbac8089d9bf0c0d6761e
Conor <conorpp@vt.edu>
2015-05-19 18:55:41 +0000

    frontend lab creator skeleton

--- volt-backend/src/main/resources/static/js/controllers.js
+++ volt-backend/src/main/resources/static/js/controllers.js
@@ -23,13 +23,24 @@ Object.size = function(obj) {
     }
     return size;
 };
-function setModel(string, model){
+function setElementModel(string, model){
     return string.replace(/##/g,model);
+}
+
+function setStepModel(element, id){
+    element.find('.step-number').text((id+1)+'');
+    return element.html(element.html().replace(/steps\[0\]/g, 'steps['+id+']'));
 }
 
 function StepElement(type,parent){
     return {
         type: type, value:null, step: parent, plusTolerance:0, negTolerance:0
+    };
+}
+
+function Step(id, sel){
+    return {
+        id: id, name:'', elements: [], currentSelect: sel
     };
 }
 
@@ -42,27 +53,46 @@ voltControllers.controller('LabCreateCntrl', ['$scope', '$http', '$compile','htm
     $scope.stepElements = ['value-question', 'graph-question', 'lab-description'];
     var numTemplates = 4;
     $scope.templates['step'] = angular.element(document.getElementById('step0')).clone();
-
-
+    var defaultSelect = $scope.stepElements[0];
 
     $scope.form = {
         name: '',
         steps: [
-            { id: 0, name:'', elements:[], currentSelect: $scope.stepElements[0] }
+            { id: 0, name:'', elements:[], currentSelect: defaultSelect }
         ]
     };
 
+    // show options only when partials arrive
     $scope.showButtons = false;
     var setupFn = function(){
         if (Object.size($scope.templates) == numTemplates){
-            console.log('showing elements');
             $scope.showButtons = true;
         }
      };
 
+    for (var i in $scope.stepElements){
+        (function(){
+            var ele = $scope.stepElements[i];
+
+            $http.get('/html/partials/'+ele+'.html')
+            .success(function(res){
+                $scope.templates[ele] = res;
+                setupFn();
+            }).error(function(){ console.log('No go for '+ele); });
+        })();
+    }
+
     $scope.addElementToStep = function(res){
-        var step = res.srcElement.parentElement.parentElement;
+
+        var step = res.target.parentElement;
+        while(step.id.indexOf('step') == -1)
+        {
+            step = step.parentElement;
+        }
+
         var id = parseInt(step.id.replace('step',''));
+
+        // get the value from dropdown
         var sel = $scope.form.steps[id].currentSelect;
 
         if (!sel){
@@ -70,10 +100,11 @@ voltControllers.controller('LabCreateCntrl', ['$scope', '$http', '$compile','htm
         }
 
         var linkFn = null;
+        // add a Step Element to the form
         $scope.form.steps[id].elements.push(new StepElement(sel, $scope.form.steps[id]));
         var indx = $scope.form.steps[id].elements.length -1;
 
-        var templ = setModel($scope.templates[sel], 'form.steps['+id+'].elements['+indx+']');
+        var templ = setElementModel($scope.templates[sel], 'form.steps['+id+'].elements['+indx+']');
         linkFn =  $compile(templ);
         var html = linkFn($scope);
         angular.element(step.children[1]).append(html);
@@ -83,30 +114,30 @@ voltControllers.controller('LabCreateCntrl', ['$scope', '$http', '$compile','htm
 
     $scope.removeElement = function(event, model){
         if (!model) return;
-        event.srcElement.parentElement.remove();
+        event.target.parentElement.remove();
         var indx = model.step.elements.indexOf(model);
         if (indx > -1){
+            // Set a tombstone to not mess up position of other elements
             model.step.elements[indx] = null;
-            console.log('removed element!');
         }
     };
 
     $scope.addStep = function(){
-        angular.element(document.getElementById('stepArea'));
+        var newStep = $scope.templates['step'].clone();
+        var id = $scope.form.steps.length;
+        newStep.id = 'step' + id;
+        newStep.attr('id', newStep.id);
+
+        $scope.form.steps.push( new Step(id, defaultSelect) );
+
+        setStepModel(newStep, id);
+        var linkFn = $compile(newStep);
+        var html = linkFn($scope);
+        angular.element(document.getElementById('stepArea'))
+                .append(html);
+
     };
 
-    for (var i in $scope.stepElements){
-        (function(){
-            var ele = $scope.stepElements[i];
-
-            $http.get('/html/partials/'+ele+'.html')
-            .success(function(res){
-                $scope.templates[ele] = res;
-                console.log('requested '+ele);
-                setupFn();
-            }).error(function(){ console.log('No go'); });
-        })();
-    }
 
 
 }]);
